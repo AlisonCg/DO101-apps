0fbd6af1c8eb8af9a0ce1fd024ab7bf7b8bcf475
Alicia Cortes <alis04cortesg@gmail.com>
2025-11-23 17:25:18 -0600

    v2: update / and /mars responses

--- express-helloworld/app.js
+++ express-helloworld/app.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const app = express();
 
-app.get('/', (req, res) => res.send('Hello World!\n'));
-app.get('/mars', (req, res) => res.send('Hello Mars!\n'));
+app.get('/', (req, res) => {
+  res.send('Hello World! v2\n');
+});
+
+app.get('/mars', (req, res) => {
+  res.send('Hello Mars! v2\n');
+});
 
 const port = 8080;
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
